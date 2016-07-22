fca1dceb6c5b6f3bafa2954b33aaac07a4ac5e5e
Will Steinmetz <willsteinmetz@me.com>
2016-07-21 21:20:11 -0400

    Updating tests

--- spec/notific8-closeallSpec.js
+++ spec/notific8-closeallSpec.js
@@ -7,7 +7,7 @@
  */
 describe('notific-closeall module', function() {
   beforeEach(function() {
-    notific8('remove');
+    notific8('destroy');
   });
 
   it('should add a closeall button to the notification container', function() {
@@ -31,5 +31,57 @@ describe('notific-closeall module', function() {
     notificationCloseAll = notification.querySelectorAll(notificationCloseAllClass);
 
     expect(notificationCloseAll.length).toEqual(1);
+  });
+
+  it('should add the theme materialish and color lollypop', function() {
+    var notification,
+      notificationContainerClass,
+      notificationCloseAll,
+      notificationCloseAllClass,
+      notificationCloseAllButtonClasses;
+
+    // enable the closeall module
+    notific8('configure', {
+      closeAll: true,
+      closeAllTheme: "materialish",
+      closeAllColor: "lollypop"
+    });
+
+    // open two notifications
+    notific8('first notification');
+    notific8('second notification');
+
+    notificationContainerClass = "." + notific8Defaults.namespace + "-container.top.right";
+    notification = document.querySelector(notificationContainerClass);
+    notificationCloseAllClass = "." + notific8Defaults.namespace + "-closeall-button";
+    notificationCloseAll = notification.querySelector(notificationCloseAllClass);
+    notificationCloseAllButtonClasses = notificationCloseAll.className.split(' ');
+
+    expect(notificationCloseAllButtonClasses.indexOf('materialish')).not.toEqual(-1);
+    expect(notificationCloseAllButtonClasses.indexOf('lollypop')).not.toEqual(-1);
+  });
+
+  it('should change the close all button text', function() {
+    var notification,
+      notificationContainerClass,
+      notificationCloseAll,
+      notificationCloseAllClass;
+
+    // enable the closeall module
+    notific8('configure', {
+      closeAll: true,
+      closeAllText: "adios!"
+    });
+
+    // open two notifications
+    notific8('first notification');
+    notific8('second notification');
+
+    notificationContainerClass = "." + notific8Defaults.namespace + "-container.top.right";
+    notification = document.querySelector(notificationContainerClass);
+    notificationCloseAllClass = "." + notific8Defaults.namespace + "-closeall-button";
+    notificationCloseAll = notification.querySelector(notificationCloseAllClass);
+
+    expect(notificationCloseAll.innerHTML.toString().trim()).toEqual("adios!");
   });
 });
